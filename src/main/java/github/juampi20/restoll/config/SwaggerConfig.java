c09ede84a151687c61b510fc0f39f170529f3d5a
Juan Pablo Garcia <juampigarcia2014@gmail.com>
2021-07-28 01:14:09 -0300

    feat(Swagger): :zap: Add Swagger UI Docs

---
+++ src/main/java/github/juampi20/restoll/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package github.juampi20.restoll.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+// Swagger URL: http://localhost:8080/swagger-ui/index.html
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                // .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("github.juampi20.restoll.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
